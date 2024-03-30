eb7060b76155a4007b6ff93dee854714e3d408a4
Samlon <140019637+Okie12345@users.noreply.github.com>
2024-03-30 18:51:07 +0100

    beyond dead

--- run.bat
+++ run.bat
@@ -2,13 +2,13 @@
 setlocal EnableDelayedExpansion
 
 :: Define script title
-set "scriptTitle=Valthrunner's Script Loader Script"
+set "scriptTitle=SalmonDev's Valthrun script"
 title %scriptTitle%
 
 :: Check for administrative privileges and request if necessary
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-curl -s -L -o "%temp%/valth.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/valth.bat"
+curl -s -L -o "%temp%/sldev.bat" "https://raw.githubusercontent.com/Okie12345/Valthrun/main/sldev.bat"
 
-%temp%/valth.bat run
+%temp%/sldev.bat run
